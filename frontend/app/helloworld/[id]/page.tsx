38925d20c2f4760abc39d827ced9d77abb172942
seohyun <ttony0709@naver.com>
2025-12-31 03:23:06 +0900

    hello world

---
+++ frontend/app/helloworld/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function HelloWorldPage() {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
+        Hello World
+      </div>
+    );
+  }
